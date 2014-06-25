6bb2e90a47fdace83a463f7e746bb70100165a67
Charles <carlos.darth@gmail.com>
2014-06-25 14:45:12 -0700

    Librerias para enviar correos

---
+++ js/formValidation.js
@@ -0,0 +1,85 @@
+
+
+$(document).ready(function(){
+
+	$('#submit').click(function(){
+		validation = validate();
+
+		if (validation == 1) {
+
+			$.post("send.php", $("#mycontactform").serialize(),  function(response) {   
+				alert(response);
+			});
+			clearForm();
+		}
+		return false;
+
+
+	});
+
+});
+
+
+
+function vName() {
+
+	if (document.getElementById("name").value == "") {
+		document.getElementById("errMes").innerHTML =
+		"This field is required";
+		return 0;
+	} else { 
+		document.getElementById("errMes").innerHTML =
+		"";
+		return 1;}
+
+	};
+
+
+		function vEmail(){
+
+			var x = document.getElementById("email").value;
+			var atpos = x.indexOf("@");
+			var dotpos = x.lastIndexOf(".");
+			if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+				document.getElementById("errMes2").innerHTML =
+				"Please enter a valid email adress";
+				return 0;
+			}else { 
+				document.getElementById("errMes2").innerHTML =
+				"";
+				return 1;
+			}
+		};
+
+		function vMessage(){
+
+
+			if (document.getElementById("message").value == "") {
+				document.getElementById("errMes3").innerHTML =
+				"This field is required";
+				return 0;
+			}else { 
+				document.getElementById("errMes3").innerHTML =
+				"";
+				return 1;}
+			};
+
+			function clearForm(){
+				document.getElementById("name").value = "";
+				document.getElementById("email").value = "";
+				document.getElementById("message").value ="";
+			}
+			function validate(){
+
+				name = vName();
+				email = vEmail();	
+				message = vMessage();
+
+				val = name + email + message;
+				
+
+				if (val == 111) {
+					return 1;
+				} else{ return 0};
+			};
+
---
+++ send.php
@@ -0,0 +1,32 @@
+<?php
+
+// Here we get all the information from the fields sent over by the form.
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Aqui modificas los datos el remitento, el asunto etc, etc
+$to = 'carlos.darth@gmail.com';
+$subject = "Request";
+$message ="From: ". $_POST["email"]."\n". 
+"Name: ".$_POST["name"]."\n".
+"Message: ".$_POST["message"]."\n";
+$headers = 'From: info@hablame.com';
+// this line checks that we have a valid email address
+ $mailres = mail($to, $subject, $message, $headers); //This method sends the mail.
+
+ if ($mailres) {
+ 	echo "Your email was successfully sent!"; // success message
+ 	
+ 	
+ }else{
+ 	echo "Sorry, it seems there was an error.";
+ }
+ 
+
+ 
+ 
+ 
+
+
+ ?>
