8f668e2ed3c50ce2ad590cf252a7a19c2fb272c9
Alan Alberto <alan_bm@hotmail.com>
2014-06-25 16:43:05 -0700

    Arregle el nombre de la base de datos al nuevo

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php 
 	define("DB_USER", "root");
 	define("DB_PASSWORD", "");
-	define("DB_DATABASE", "vendomasbarato");
+	define("DB_DATABASE", "vendomasbara");
 	define("DB_SERVER", "localhost");
  ?>
