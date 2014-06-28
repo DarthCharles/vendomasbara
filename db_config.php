79bbd0330de34818319f5ce62d5eea8bfd5d3ef7
Alan Alberto <alan_bm@hotmail.com>
2014-06-28 12:40:12 -0700

    Ignore DB_Config.php

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php 
 	define("DB_USER", "root");
 
-	define("DB_PASSWORD", "carlosh12");
+	define("DB_PASSWORD", "");
 	define("DB_DATABASE", "vendomasbara");
 
 	define("DB_SERVER", "localhost");
