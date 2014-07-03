5ad1165d7b4685635af4eeef660920da09bb19bd
Charles <carlos.darth@gmail.com>
2014-07-02 20:35:40 -0700

    Estilo al paginador
    Le puse el estilo de bootstrap al paginador con los colorsitos de la
    pagina.

--- index.php
+++ index.php
@@ -69,101 +69,101 @@
         if ($login == "false") {
          loginErrMess();
          
-        }             
-            $registros = 6;
+       }             
+       $registros = 6;
 
-            if(isset($_GET["pagina"])){
-              $pagina = $_GET["pagina"];
-            }else{
-              $pagina=0;
-            }
+       if(isset($_GET["pagina"])){
+        $pagina = $_GET["pagina"];
+      }else{
+        $pagina=0;
+      }
 
-            
-            
-            if ($pagina==0) { 
-                  $inicio = 0; 
-                  $pagina = 1; 
-            } 
-            else { 
-                  $inicio = ($pagina - 1) * $registros; 
-            } 
-            
 
-             $db = new DB_CONNECT(); 
-             $query=("SELECT * FROM Publicacion ORDER BY idPublicacion DESC LIMIT ".$inicio.",".$registros);            
-             $resultados = $db->query($query);
 
-             $query2=("SELECT COUNT(*) AS num FROM Publicacion");
+      if ($pagina==0) { 
+        $inicio = 0; 
+        $pagina = 1; 
+      } 
+      else { 
+        $inicio = ($pagina - 1) * $registros; 
+      } 
 
-        $data = $db->query($query2); 
 
-        $row=mysqli_fetch_assoc($data);
-        $total_registros=$row['num'];
+      $db = new DB_CONNECT(); 
+      $query=("SELECT * FROM Publicacion ORDER BY idPublicacion DESC LIMIT ".$inicio.",".$registros);            
+      $resultados = $db->query($query);
 
-        
+      $query2=("SELECT COUNT(*) AS num FROM Publicacion");
 
-        $total_paginas = ceil($total_registros / $registros);             
+      $data = $db->query($query2); 
 
-  if($total_registros) {
-  
-    while($res=mysqli_fetch_array($resultados)) {
-      
-      $titulo = $res["Titulo"];
-                  $precio = $res["Precio"];
-                  $imagen = $res["Imagen"];
-                 
-                  $idPublicacion = $res["idPublicacion"];
+      $row=mysqli_fetch_assoc($data);
+      $total_registros=$row['num'];
 
-                  echo "<div class='col-md-4'  id='producto'>";
-                  echo "<center><a href='publicacion.php?idPublicacion=".$idPublicacion."'><img src='".$imagen."' class='img-rounded '></a></center>";
-                  echo "<center><h4>".$titulo."</h4></center>";
-                  echo "<center><h5>Precio: $".$precio."</h5></center>";
-                  echo "</div>";
-      
-    }
-    
-  } else {
-    echo "<font color='darkgray'>(sin resultados)</font>";
-  }
-   
-   echo "<div class='col-md-12'>";
-  mysqli_free_result($resultados);        
-  
-  if($total_registros) {
-    
-    echo "<center>";
-    
-    if(($pagina - 1) > 0) {
-      echo "<a href='index.php?pagina=".($pagina-1)."'>< Anterior</a> ";
-    }
-    
-    for ($i=1; $i<=$total_paginas; $i++){ 
-      if ($pagina == $i) 
-        echo "<b>".$pagina."</b> "; 
-      else
-        echo "<a href='index.php?pagina=$i'>$i</a> "; 
-    }
-    
-    if(($pagina + 1)<=$total_paginas) {
-      echo " <a href='index.php?pagina=".($pagina+1)."'>Siguiente ></a>";
-    }
-    
-    echo "</center>";
-    echo"</div>";
-    
-  }
+
+
+      $total_paginas = ceil($total_registros / $registros);             
+
+      if($total_registros) {
+
+        while($res=mysqli_fetch_array($resultados)) {
+
+          $titulo = $res["Titulo"];
+          $precio = $res["Precio"];
+          $imagen = $res["Imagen"];
+
+          $idPublicacion = $res["idPublicacion"];
+
+          echo "<div class='col-md-4'  id='producto'>";
+          echo "<center><a href='publicacion.php?idPublicacion=".$idPublicacion."'><img src='".$imagen."' class='img-rounded '></a></center>";
+          echo "<center><h4>".$titulo."</h4></center>";
+          echo "<center><h5>Precio: $".$precio."</h5></center>";
+          echo "</div>";
+
+        }
+
+      } else {
+        echo "<font color='darkgray'>(sin resultados)</font>";
+      }
+
+      echo "<div class='col-md-12'>";
+      mysqli_free_result($resultados);        
+
+      if($total_registros) {
+
+        echo "<center>";
+        echo "<ul class='pagination' id='paginador'>";
+        if(($pagina - 1) > 0) {
+
+          echo "<li><a href='index.php?pagina=".($pagina-1)."'>< Anterior</a> </li>";
+        }
+
+        for ($i=1; $i<=$total_paginas; $i++){ 
+          if ($pagina == $i) 
+            echo "<li id='pagActive'><a><strong>".$pagina."</strong></a></li> "; 
+          else
+            echo "<li ><a href='index.php?pagina=$i'>$i</a></li> "; 
+        }
+
+        if(($pagina + 1)<=$total_paginas) {
+          echo "<li><a href='index.php?pagina=".($pagina+1)."'>Siguiente ></a></li>";
+        }
+        echo "</ul>";
+        echo "</center>";
+        echo"</div>";
+
+
+      }
       ?>
 
-     </div>
+    </div>
+</div>
 
 
 
 
 
 
-
-
-   </div>
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
